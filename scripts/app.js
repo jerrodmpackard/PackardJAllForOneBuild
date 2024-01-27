b9beee36ea2826d28eb3e43915bd4306e878f786
jerrodmpackard <jerrodmpackard@gmail.com>
2024-01-26 19:38:28 -0800

    made text sizes bigger

--- scripts/app.js
+++ scripts/app.js
@@ -42,11 +42,196 @@ let restaurantPickerFastFoodButton = document.getElementById("restaurantPickerFa
 let restaurantPickerPizzaButton = document.getElementById("restaurantPickerPizzaButton");
 let restaurantPickerRestaurantButton = document.getElementById("restaurantPickerRestaurantButton");
 
-sayHelloButton.addEventListener("click", function() {
-    // run API call for say hello endpoint
+let userName;
+let number1;
+let number2;
+let wakeUpTime;
+let heroName;
+let planetName;
+let speciesName;
+let pluralNoun;
+let singularNoun1;
+let singularNoun2;
+let verb1;
+let verb2;
+let adj1;
+let adj2;
+let something;
+let question;
 
-    // const promise = await fetch("");
-    // const data = await promise.json();
-    
-    // sayHelloDiv.textContent = `Hello, {data}. Nice to meet you!`;
-});
+const sayHelloCall = async (userName) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/SayHello/SayHello/${userName}`);
+    const data = await promise.text();
+
+    sayHelloDiv.innerText = data;
+}
+
+if (sayHelloButton) {
+    sayHelloButton.addEventListener("click", function () {
+        userName = sayHelloInput.value;
+        sayHelloCall(userName);
+    });
+}
+
+const sumTwoNumbersCall = async (number1, number2) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/SumTwoNumbers/SumTwoNumbers/${number1}/${number2}`);
+    const data = await promise.text();
+
+    sumTwoNumbersDiv.innerText = data;
+}
+
+if (sumTwoNumbersButton) {
+    sumTwoNumbersButton.addEventListener("click", function () {
+        number1 = sumTwoNumbersInput1.value;
+        number2 = sumTwoNumbersInput2.value;
+        sumTwoNumbersCall(number1, number2);
+    });
+}
+
+const askingQuestionsCall = async (userName, wakeUpTime) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/AskingQuestions/AskingQuestions/${userName}/${wakeUpTime}`);
+    const data = await promise.text();
+
+    askingQuestionsDiv.innerText = data;
+}
+
+if(askingQuestionsButton){
+    askingQuestionsButton.addEventListener("click", function () {
+        userName = askingQuestionsNameInput.value;
+        wakeUpTime = askingQuestionsTimeInput.value;
+        askingQuestionsCall(userName, wakeUpTime);
+    })
+}
+
+const greaterOrLessCall = async (number1, number2) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/GreaterOrLess/GreaterOrLess/${number1}/${number2}`);
+    const data = await promise.text();
+
+    greaterOrLessDiv.innerText = data;
+}
+
+if(greaterOrLessButton){
+    greaterOrLessButton.addEventListener("click", function () {
+        number1 = greaterOrLessNumber1Input.value;
+        number2 = greaterOrLessNumber2Input.value;
+        greaterOrLessCall(number1, number2);
+    })
+}
+
+const madLibsCall = async (heroName, planetName, speciesName, pluralNoun, singularNoun1, singularNoun2, verb1, verb2, adj1, adj2) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/MadLibs/MadLibs/${heroName}/${planetName}/${speciesName}/${pluralNoun}/${singularNoun1}/${singularNoun2}/${verb1}/${verb2}/${adj1}/${adj2}`);
+    const data = await promise.text();
+
+    madLibsDiv.innerText = data;
+}
+
+if(madLibsButton){
+    madLibsButton.addEventListener("click", function() {
+        heroName = madLibsHeroName.value;
+        planetName = madLibsPlanetName.value;
+        speciesName = madLibsSpeciesName.value;
+        pluralNoun = madLibsPluralNoun.value;
+        singularNoun1 = madLibsSingularNoun1.value;
+        singularNoun2 = madLibsSingularNoun2.value;
+        verb1 = madLibsVerb1.value;
+        verb2 = madLibsVerb2.value;
+        adj1 = madLibsVerb1.value;
+        adj2 = madLibsVerb2.value;
+        madLibsCall(heroName, planetName, speciesName, pluralNoun, singularNoun1, singularNoun2, verb1, verb2, adj1, adj2);
+    })
+}
+
+const oddOrEvenCall = async (number1) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/OddOrEven/OddOrEven/${number1}`);
+    const data = await promise.text();
+
+    oddOrEvenDiv.innerText = data;
+}
+
+if(oddOrEvenButton){
+    oddOrEvenButton.addEventListener("click", function() {
+        number1 = oddOrEvenNumberInput.value;
+        oddOrEvenCall(number1);
+    })
+}
+
+const reverseWordsCall = async (something) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/ReverseWords/ReverseWords/${something}`);
+    const data = await promise.text();
+
+    reverseWordsDiv.innerText = data;
+}
+
+if(reverseWordsButton){
+    reverseWordsButton.addEventListener("click", function() {
+        something = reverseWordsInput.value;
+        reverseWordsCall(something);
+    })
+}
+
+const reverseNumbersCall = async (number1) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/ReverseNumbers/ReverseNumbers/${number1}`);
+    const data = await promise.text();
+
+    reverseNumbersDiv.innerText = data;
+}
+
+if(reverseNumbersButton){
+    reverseNumbersButton.addEventListener("click", function() {
+        number1 = reverseNumbersInput.value;
+        reverseNumbersCall(number1);
+    })
+}
+
+const magic8BallCall = async (question) => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/Magic8Ball/Magic8Ball/${question}`);
+    const data = await promise.text();
+
+    magic8BallDiv.innerText = data;
+}
+
+if(magic8BallButton){
+    magic8BallButton.addEventListener("click", function () {
+        question = magic8BallQuestionInput.value;
+        magic8BallCall(question);
+    })
+}
+
+const fastFoodCall = async () => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/RestaurantPicker/RestaurantPicker/fast food`);
+    const data = await promise.text();
+
+    restaurantPickerDiv.innerText = data;
+}
+
+if(restaurantPickerFastFoodButton){
+    restaurantPickerFastFoodButton.addEventListener("click", function() {
+        fastFoodCall();
+    })
+}
+
+const pizzaCall = async () => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/RestaurantPicker/RestaurantPicker/pizza`);
+    const data = await promise.text();
+
+    restaurantPickerDiv.innerText = data;
+}
+
+if(restaurantPickerPizzaButton){
+    restaurantPickerPizzaButton.addEventListener("click", function () {
+        pizzaCall();
+    })
+}
+
+const restaurantCall = async () => {
+    const promise = await fetch(`https://packardjallforonewebapi.azurewebsites.net/RestaurantPicker/RestaurantPicker/restaurant`);
+    const data = await promise.text();
+
+    restaurantPickerDiv.innerText = data;
+}
+
+if(restaurantPickerRestaurantButton){
+    restaurantPickerRestaurantButton.addEventListener("click", function() {
+        restaurantCall();
+    })
+}
